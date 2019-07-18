42e717952667b196ed376dffd2030b85a2370776
TyhGB <30275349+TyhGB@users.noreply.github.com>
2019-07-18 15:35:35 +0800

    initial pod

---
+++ B_Category/Classes/CTMediator+B.h
@@ -0,0 +1,17 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by TyhOS on 2019/7/18.
+//  Copyright © 2019 上海佰量网络科技. All rights reserved.
+//
+
+#import "CTMediator.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (B)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ B_Category/Classes/CTMediator+B.m
@@ -0,0 +1,13 @@
+//
+//  CTMediator+B.m
+//  B_Category
+//
+//  Created by TyhOS on 2019/7/18.
+//  Copyright © 2019 上海佰量网络科技. All rights reserved.
+//
+
+#import "CTMediator+B.h"
+
+@implementation CTMediator (B)
+
+@end
